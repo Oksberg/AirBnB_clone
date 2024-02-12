7f12b49d80732799dfcd59b9f9badb79ff899a65
Josephnkansah <jnkansahoseph4@gmail.com>
2024-02-11 16:11:35 -0800

    py

--- models/user.py
+++ models/user.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-Module for the User class.
-"""
+"""Module that defines the User class"""
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    class User that handles users' information
-    """
+    """Class that defines User instance attributes"""
+
     email = ""
     password = ""
     first_name = ""
